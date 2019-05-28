fcdbfb6c79b20b4566d1ed610dce335aef307e57
Sandra Tourjman <sandratourjman@Sandras-MacBook-Pro.local>
2019-05-28 16:11:35 -0500

    assignment 7

---
+++ spec/unit/topic_spec.js
@@ -0,0 +1,90 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topics;
+const Post = require("../../src/db/models").Post;
+
+describe("Topic", () => {
+	beforeEach((done) =>{
+		this.topic;
+    	this.post;
+    	sequelize.sync({force: true}).then((res) => {
+	      Topic.create({
+	        title: "Expeditions to Alpha Centauri",
+	        description: "A compilation of reports from recent visits to the star system."
+	      })
+	      .then((topic) => {
+	        this.topic = topic;
+
+	        Post.create({
+	          title: "My first visit to Proxima Centauri b",
+	          body: "I saw some rocks.",
+	          topicId: this.topic.id
+	        })
+	        .then((post) => {
+	          this.post = post;
+	          done();
+	        });
+	      })
+	      .catch((err) => {
+	        console.log(err);
+	        done();
+	      });
+	    });
+	});
+
+	describe("#create()", () => {
+		it("should create a topic object with a title and description", (done) => {
+			Topic.create({
+				title: "Expeditions to Alpha Centauri",
+				description: "A compilation of reports from recent visits to the star system"
+			})
+			.then((topic) => {
+				expect(topic.title).toBe("Expeditions to Alpha Centauri");
+				expect(topic.description).toBe("A compilation of reports from recent visits to the star system");
+				done();
+			})
+			.catch((err) => {
+				console.log(err);
+				done();
+			});
+		});
+
+		it("should not create a topic with missing title or description", (done) => {
+			Topic.create({
+				title: "Expeditions to Alpha Centauri",
+				description: "A compilation of reports from recent visits to the star system"
+
+			})
+			.then((topic) => {
+				done();
+			})
+			.catch((err) => {
+				expect(err.message).toContain("Topic.title cannot be null");
+				expect(err.message).toContain("Topic.description cannot be null");
+				done();
+			});
+		});
+	});
+
+	describe("#getPosts()", () => {
+		it("should add a post to a topic and return the associated post", (done) => {
+			Post.create({
+				title: "My first visit to Proxima Centauri b",
+	          	body: "I saw some rocks.",
+	          	topicId: this.topic.id
+			})
+			.then((post) => {
+				this.topic.getPosts()
+				.then((posts) => {
+					expect(this.topic.id).toBe(post.topicId);
+                  	expect(this.topic.id).toBe(posts[1].topicId);
+					done();
+				});
+			})
+			.catch((err) => {
+				console.log(err);
+				done();
+			});
+		});
+	});
+
+});
