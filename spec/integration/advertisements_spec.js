2a185aa91890de5c967eb70d121c1511b0c566c9
Sandra Tourjman <sandratourjman@Sandras-MacBook-Pro.local>
2019-05-28 23:02:47 -0500

    assignment 8 add flair

--- spec/integration/advertisements_spec.js
+++ spec/integration/advertisements_spec.js
@@ -3,7 +3,7 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/topics";
 
 const sequelize = require("../../src/db/models/index").sequelize;
-const Topic = require("../../src/db/models").Topic;
+const Topic = require("../../src/db/models").Topics;
 const Post = require("../../src/db/models").Post;
 
 describe("routes : posts", () => {
